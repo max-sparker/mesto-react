33cff07216b92d5d907667265e91a568371fc076
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-11-01 17:52:07 +0500

    fix

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function ImagePopup({card, onClose, isOpen}) {
+function ImagePopup({onClose, isOpen}) {
   return (
     <section className={`popup popup_type_image ${isOpen ? 'popup__opened' : ''}`}>
       <div className="popup__overlay" />
       <div className="popup__container">
         <button className="popup__close-btn" type="button" aria-label="Закрыть окно" onClick={onClose} />
         <figure className="popup__full">
-          <img className="popup__image" src={card.link} alt={card.name} />
-          <figcaption className="popup__desc">{card.name}</figcaption>
+          <img className="popup__image" src="" alt="" />
+          <figcaption className="popup__desc"></figcaption>
         </figure>
       </div>
     </section>
