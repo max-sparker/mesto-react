7ae6e6d277b3f629bc7cc7c7e50973e3695f5c7f
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-11-02 22:34:04 +0500

    feat: вынесение компонента Card

---
+++ src/components/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Card ({card, onCardClick}) {
+
+  function handldeClick() {
+    onCardClick(card);
+  }
+
+  return(
+    <article className="card">
+      <img className="card__image" src={card.link} alt={card.name} onClick={handldeClick}/>
+      <button className="card__remove-btn" type="button" aria-label="Удалить" />
+      <div className="card__title">
+        <h2 className="card__name">{card.name}</h2>
+        <div className="card__like-container">
+          <button className="card__like-btn" type="button" aria-label="Мне нравится" />
+          <p className="card__like-counter">{card.likes.length}</p>
+        </div>
+      </div>
+    </article>
+  );
+
+}
+
+export default Card;
